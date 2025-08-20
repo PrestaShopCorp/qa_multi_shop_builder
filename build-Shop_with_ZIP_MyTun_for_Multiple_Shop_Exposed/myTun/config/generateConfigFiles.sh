b9438a00a7fe719236b9e21e389e3c6f3a66a7cf
ervinpresta <ervin.klicic@prestashop.com>
2025-08-20 11:09:34 +0200

    change docker compose and Makefile

--- build-Shop_with_ZIP_MyTun_for_Multiple_Shop_Exposed/myTun/config/generateConfigFiles.sh
+++ build-Shop_with_ZIP_MyTun_for_Multiple_Shop_Exposed/myTun/config/generateConfigFiles.sh
@@ -15,17 +15,22 @@ echo "$credentials_json_content" > "./myTun/config/mytun-credentials.json"
 echo "credentials.json file created !"
 
 ############ Create config.yml file ###########
-touch "./myTun/config/mytun-config.yml"
+CGF="./myTun/config/mytun-config.yml"
+
+if [ ! -f "$CGF" ]; then
+  echo "tunnel: \"$TUNNEL_ID\"" >> "$CGF"
+  echo "credentials-file: /credentials.json" >> "$CGF"
+  echo "ingress:" >> "$CGF"
+fi
 # Set config in yaml
-config_yaml_content="tunnel: \"$TUNNEL_ID\"
-credentials-file: /credentials.json
-ingress:
-  - hostname: \"$PS_DOMAIN\"
-    service: http://prestashop:80
-    originRequest:
-      httpHostHeader: \"$PS_DOMAIN\"
-  - service: http_status:404"
+if ! grep -q "hostname: \"$PS_DOMAIN\"" "$CGF"; then
+    sed -i '' '/service: http_status:404/d' "$CGF"
+    echo "  - hostname: \"$PS_DOMAIN\"" >> "$CGF"
+    echo "    service: http://shop${SHOP_ID}-prestashop:80" >> "$CGF"
+    echo "    originRequest:" >> "$CGF"  
+    echo "      httpHostHeader: \"$PS_DOMAIN\"" >> "$CGF"
+    echo "  - service: http_status:404" >> "$CGF"
+fi
 
 # Print in file
-echo "$config_yaml_content" > "./myTun/config/mytun-config.yml"
 echo "config.yml file created !"
